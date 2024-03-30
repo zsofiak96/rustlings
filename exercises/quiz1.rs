40f12cb3ce48f6b918e290dee793197e44876159
zsofiak96 <kalosi.zsofia@gmail.com>
2024-03-30 06:47:09 +0100

    solve: Add 'quiz1' solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples <= 40 {
+        num_apples * 2
+    } else {
+        num_apples
+    }
+}
 
 // Don't modify this function!
 #[test]
