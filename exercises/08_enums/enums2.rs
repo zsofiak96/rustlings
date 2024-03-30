7ce1180921dc52bcd9213ec7af5d41808fe20568
zsofia <zk@raicoon.com>
2024-03-30 18:45:08 +0100

    solve: Add '08_enums' solutions

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -3,11 +3,13 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    ChangeColor(u16, u16, u16),
+    Echo(String),
+    Move{x: i8, y: i8}, 
+    Quit,
 }
 
 impl Message {
