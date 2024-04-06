a7670df58ed6ade2c3a6497a7eb00bcc80670569
zsofiak96 <kalosi.zsofia@gmail.com>
2024-04-06 07:53:22 +0200

    solve: Add '19_smart_pointers' solutions

--- exercises/19_smart_pointers/arc1.rs
+++ exercises/19_smart_pointers/arc1.rs
@@ -12,7 +12,7 @@
 // The eighth thread (offset 7), will sum 7, 15, 23, ...
 //
 // Because we are using threads, our values need to be thread-safe.  Therefore,
-// we are using Arc.  We need to make a change in each of the two TODOs.
+// we are using Arc. We need to make a change in each of the two TODOs.
 //
 // Make this code compile by filling in a value for `shared_numbers` where the
 // first TODO comment is, and create an initial binding for `child_numbers`
@@ -21,19 +21,17 @@
 //
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = Arc::clone(&shared_numbers);
         joinhandles.push(thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
